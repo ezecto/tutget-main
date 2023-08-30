9262ad6887325d219ae9400ed525845db3ca5630
jhtoh <jhtoh@outlook.com>
2023-08-30 18:04:23 +0800

    first commit

---
+++ src/main/java/com/tutget/tutgetmain/TutgetMainApplication.java
@@ -0,0 +1,25 @@
+package com.tutget.tutgetmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TutgetMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutgetMainApplication.class, args);
+	}
+
+	@GetMapping("/app")
+	public String test(){
+		return "Hello world!";
+	}
+
+
+
+}
---
+++ src/test/java/com/tutget/tutgetmain/TutgetMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutget.tutgetmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutgetMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
